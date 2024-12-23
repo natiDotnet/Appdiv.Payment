40cf91dce097cae23107d6f98be9bf650e690284
Natnael Yirga <NatiDotnet@gmail.com>
2024-12-23 13:18:24 +0300

    change cbe response namespace

---
+++ Appdiv.Payment.CBEbirr/Services/CBETransactionCustomMessage.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml;
+using SoapCore;
+
+namespace Appdiv.Payment.CBEbirr.Services;
+
+// ReSharper disable once InconsistentNaming
+public class CBETransactionCustomMessage : CustomMessage
+{
+    private const string EnvelopeNamespace = "http://www.w3.org/2003/05/soap-envelope";
+    private const string EnvelopeShortName = "env";
+
+    protected override void OnWriteStartEnvelope(XmlDictionaryWriter writer)
+    {
+        writer.WriteStartElement(EnvelopeShortName, "Envelope", EnvelopeNamespace);
+        writer.WriteXmlnsAttribute(EnvelopeShortName, EnvelopeNamespace);
+        writer.WriteXmlnsAttribute("ns1", Namespace.AT);
+    }
+    
+    protected override void OnWriteStartBody(XmlDictionaryWriter writer)
+    {
+        writer.WriteStartElement("Body", EnvelopeNamespace);
+    }
+
+}
--- Appdiv.Payment.CBEbirr/Services/CBEbirrCustomMessage.cs
+++ Appdiv.Payment.CBEbirr/Services/CBEbirrCustomMessage.cs
@@ -1,4 +1,7 @@
 ﻿using System.Xml;
+using Appdiv.Payment.Telebirr.Helper;
+using Appdiv.Payment.Telebirr.Requests;
+using Appdiv.Payment.Telebirr.Responses;
 using SoapCore;
 
 namespace Appdiv.Payment.CBEbirr.Services;
@@ -7,18 +10,30 @@ namespace Appdiv.Payment.CBEbirr.Services;
 public class CBEbirrCustomMessage : CustomMessage
 {
     private const string EnvelopeNamespace = "http://www.w3.org/2003/05/soap-envelope";
-    private const string EnvelopeShortName = "env";
+    private const string EnvelopeShortName = "SOAP-ENV";
 
     protected override void OnWriteStartEnvelope(XmlDictionaryWriter writer)
     {
         writer.WriteStartElement(EnvelopeShortName, "Envelope", EnvelopeNamespace);
         writer.WriteXmlnsAttribute(EnvelopeShortName, EnvelopeNamespace);
-        writer.WriteXmlnsAttribute("ns1", Namespace.AT);
+        writer.WriteXmlnsAttribute("ns1", Telebirr.Namespace.C2B);
     }
     
     protected override void OnWriteStartBody(XmlDictionaryWriter writer)
     {
         writer.WriteStartElement("Body", EnvelopeNamespace);
+    }
+
+    protected override void OnWriteBodyContents(XmlDictionaryWriter writer)
+    {
+        var apis = new[] { nameof(C2BPaymentConfirmationResult), nameof(C2BPaymentValidationResult), nameof(C2BPaymentQueryResult) };
+        var api = apis.FirstOrDefault(key => Message.ToString().Contains(key));
+        if (api is not null)
+        {
+            writer.WriteStartElement("c2bpayment", api, Telebirr.Namespace.C2B);
+        }
+        using var bodyReader = Message.GetReaderAtBodyContents();
+        XmlHelper.WriteXmlNode(bodyReader, writer, false);
     }
 
 }

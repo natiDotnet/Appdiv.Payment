c5fad606f2a37f984fbcbd5476d065dc9fd782ed
Natnael Yirga <NatiDotnet@gmail.com>
2024-12-24 18:15:20 +0300

    remove unnecessary contracts cbe

--- Appdiv.Payment.CBEbirr/ICBEbirrPayment.cs
+++ Appdiv.Payment.CBEbirr/ICBEbirrPayment.cs
@@ -1,12 +1,9 @@
 using Appdiv.Payment.CBEbirr.Requests;
 using Appdiv.Payment.CBEbirr.Responses;
-using Appdiv.Payment.Telebirr.Requests;
-using Appdiv.Payment.Telebirr.Responses;
 
 namespace Appdiv.Payment.CBEbirr;
+
 public interface ICBEbirrPayment
 {
     Task<ApplyTransactionResponse> PaymentQuery(ApplyTransactionRequest request);
-    Task<C2BPaymentValidationResult> PaymentValidation(C2BPaymentValidationRequest request);
-    Task<C2BPaymentConfirmationResult> PaymentComfirmation(C2BPaymentConfirmationRequest request);
 }
--- Appdiv.Payment.Telebirr/Services/ITelebirrService.cs
+++ Appdiv.Payment.Telebirr/Services/ITelebirrService.cs
@@ -1,19 +1,25 @@
 ﻿using System.ServiceModel;
-using System.Xml.Serialization;
 using Appdiv.Payment.Telebirr.Requests;
 using Appdiv.Payment.Telebirr.Responses;
 
+// ReSharper disable InconsistentNaming
+
 namespace Appdiv.Payment.Telebirr.Services;
+
 [ServiceContract(Namespace = Namespace.C2B)]
 public interface ITelebirrService
 {
     [OperationContract]
-    Task<C2BPaymentQueryResult> C2BPaymentQueryRequest(string TransType, string TransID, string TransTime, string BusinessShortCode, string BillRefNumber, string MSISDN);
+    Task<C2BPaymentQueryResult> C2BPaymentQueryRequest(string TransType, string TransID, string TransTime,
+        string BusinessShortCode, string BillRefNumber, string MSISDN);
 
     [OperationContract(Action = "ValidateC2BPayment")]
-    Task<C2BPaymentValidationResult> C2BPaymentValidationRequest(string BillRefNumber, string TransType, string TransID, string TransTime, decimal TransAmount, string BusinessShortCode, string MSISDN, [XmlElement("KYCInfo")] KYCInfo[] KYCInfo);
+    [XmlSerializerFormat]
+    Task<C2BPaymentValidationResult> C2BPaymentValidationRequest(string BillRefNumber, string TransType, string TransID,
+        string TransTime, decimal TransAmount, string BusinessShortCode, string MSISDN, KYCInfo[] KYCInfo);
 
     [OperationContract(Action = "ConfirmC2BPayment")]
-    Task<C2BPaymentConfirmationResult> C2BPaymentConfirmationRequest(string BillRefNumber, string TransType, string TransID, string TransTime, decimal TransAmount, string BusinessShortCode, string MSISDN, KYCInfo[] KYCInfo);
-
+    Task<C2BPaymentConfirmationResult> C2BPaymentConfirmationRequest(string BillRefNumber, string TransType,
+        string TransID, string TransTime, decimal TransAmount, string BusinessShortCode, string MSISDN,
+        KYCInfo[] KYCInfo);
 }

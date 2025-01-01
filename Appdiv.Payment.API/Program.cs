68860da8eff36fb8d01053dfab20932f9a752866
Natnael Yirga <NatiDotnet@gmail.com>
2025-01-01 14:18:38 +0300

    feat: integrate AwashBank client and add token retrieval endpoint

--- Appdiv.Payment.API/Program.cs
+++ Appdiv.Payment.API/Program.cs
@@ -1,6 +1,9 @@
 using Appdiv.Payment.API;
 using Appdiv.Payment.CBE;
 using Appdiv.Payment.Telebirr;
+using Appdiv.Payment.AwashBank;
+using Appdiv.Payment.AwashBank.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +16,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCBEbirr<CBEPayment>();
 builder.Services.AddTelebirr<TelebirrPayment>();
+builder.Services.AddAwashClient(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,9 +29,16 @@ if (app.Environment.IsDevelopment())
 app.UseCBEbirr();
 app.UseTelebirr();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
+app.UseAwashEndpoint();
+// app.UseEndpoints(endpoints => endpoints.UseAwashEndpoint());
 
 app.MapControllers();
+
+app.MapGet("/awash", async (IAwashClient client) =>
+{
+    var response = await client.GetTokenAsync(new Credential("username", "password"));
+    return response.Token;
+});
 
 app.Run();
